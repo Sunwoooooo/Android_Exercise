65617062728ab04d3312b0202dc9cce1a09dd448
Sunwoo <psw6394@naver.com>
2017-11-14 19:58:50 +0900

    MyActivity 실습 액티비티 파일

---
+++ MyActivity/myactivity/MyNextActivity.java
@@ -0,0 +1,13 @@
+package com.example.sunwoo.myactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyNextActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_next);
+    }
+}
